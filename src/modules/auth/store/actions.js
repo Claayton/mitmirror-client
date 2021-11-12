cd931b528043a3fd1b7302d7402961acfcc9125f
Clayton Garcia <claaytongarcia@gmail.com>
2021-11-12 13:29:29 -0300

    feature: Configurada toda a parte de request de API para o auth do usuario

--- src/modules/auth/store/actions.js
+++ src/modules/auth/store/actions.js
@@ -1,26 +1,70 @@
-import * as types from './mutation-types'
 import axios from 'axios'
+import * as storage from '../storage'
+import * as types from './mutation-types'
 
 export const ActionDoLogin = async ({ dispatch }, payload) => {
 
-  return await axios.post('https://mitmirror.herokuapp.com/api/auth/', {
-    email: payload.email,
-    password: payload.password
+  return await axios({
+    method: 'post',
+    url: 'https://mitmirror.herokuapp.com/api/auth/',
+    data: {
+      email: payload.email,
+      password: payload.password
+    }
   }).then((response) => {
 
     const dataResponse = response.data;
     
-    dispatch('ActionSetUser', 'Provisório-Bolsonaro')
+    storage.setLocalCurrentUserId(dataResponse.user.id)
+    storage.setLocalToken(dataResponse.Authorization)
     dispatch('ActionSetToken', dataResponse.Authorization)
+    dispatch('ActionSetUser', dataResponse.user)
+    dispatch('ActionSetCurrentUserId', dataResponse.user.id)
+  })
+}
 
-    // const status = response.status;
+export const ActionCheckToken = ({ dispatch, state }) => {
+  if (state.token && state.user) {
+    return Promise.resolve(state.token)
+  }
+  const token = storage.getLocalToken()
+  dispatch('ActionSetToken', token)
+  
+  if (!token) {
+    return Promise.reject(new Error('Token Inválido'))
+  }
+  
+  return dispatch('ActionLoadSession')
+}
 
-    // this.token = dataResponse.Authorization
-    // this.userId = dataResponse.id
+export const ActionLoadSession = ({ dispatch }) => {
+  return new Promise ((resolve, reject) => {
+    
+    try {
+      const currentId = storage.getLocalCurrentUserId()
+      const currentToken = storage.getLocalToken();
+      axios({
+        method: 'get',
+        url: `https://mitmirror.herokuapp.com/api/users/${currentId}/`,
+        headers: {'Authorization': `Bearer ${currentToken}`}
+      })
+      .then((response) => {
+      const dataResponse = response.data.data
+          
+      dispatch('ActionSetUser', dataResponse)
+      dispatch('ActionSetCurrentUserId', dataResponse.id)
+      resolve()
+      })
+    } catch (error) {
+  
+      if (error == 'Error: Request failed with status code 500') {
+        console.log('Deu errinho de server mas ja ta tudo bem!, haha')
+        ActionLoadSession()
+      }
 
-    // sessionStorage.current_token = this.token;
-    // sessionStorage.current_user_id = this.userId;
-
+      dispatch('ActionSignOut')
+      reject(error)
+    }
   })
 }
 
@@ -28,28 +72,18 @@ export const ActionSetUser = ({ commit }, payload) => {
     commit(types.SET_USER, payload)
 }
 
+export const ActionSetCurrentUserId = ({ commit }, payload) => {
+  commit(types.SET_CURRENT_USER_ID, payload)
+}
+
 export const ActionSetToken = ({ commit }, payload) => {
   commit(types.SET_TOKEN, payload)
 }
 
-/*
-async readUser() {
-    if (sessionStorage.current_user_id) {
-      const id = sessionStorage.current_user_id
-      await axios.get(`https://mitmirror.herokuapp.com/api/users/${id}/`, {
-      }).then((response) => {
-        const responseData = response.data.data
-        this.state.isActive = true
-        this.state.userData.name = responseData.name
-        this.state.userData.userName = responseData.username
-        this.state.userData.email = responseData.email
-        console.log(`O usuário ${this.state.userData.name} esta ativo!`)
-      }).catch((error) => {
-        if (error == 'Error: Request failed with status code 500') {
-          this.readUser()
-        }
-      });
-    } else {
-      this.state.isActive = false
-    }
-}*/ 
+export const ActionSignOut = ({ dispatch }) => {
+  storage.deleteLocalToken()
+  storage.deleteLocalCurrentUserId()
+  dispatch('ActionSetUser', {})
+  dispatch('ActionSetToken', '')
+  dispatch('ActionSetCurrentUserId', '')
+}
