d2f3a2e67dfee34869e1601a65fc5e96151a69d2
Clayton Garcia <claaytongarcia@gmail.com>
2021-11-12 13:26:41 -0300

    feature: Adicionado getters no store

---
+++ src/modules/auth/store/getters.js
@@ -0,0 +1,3 @@
+export const hasToken = ({ token }) => !!token
+export const hasUser = ({ user }) => !!user.id
+export const hasUserId = ({ current_user_id }) => !!current_user_id
--- src/modules/auth/store/index.js
+++ src/modules/auth/store/index.js
@@ -1,10 +1,12 @@
 import * as actions from './actions'
 import state from './state'
 import mutations from './mutations'
+import * as getters from './getters'
 
 export default {
     state,
     actions, 
+    getters,
     mutations,
     namespaced: true
 }
