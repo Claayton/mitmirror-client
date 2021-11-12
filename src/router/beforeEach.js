67b42ce391776866faf38c6ac87be5b648c372a2
Clayton Garcia <claaytongarcia@gmail.com>
2021-11-12 13:25:22 -0300

    feature: Adicionado modulo beforeEach para administrar as rotas

---
+++ src/router/beforeEach.js
@@ -0,0 +1,33 @@
+import store from '../store'
+
+export default async (to, from , next) => {
+    document.title = `MitMirror - ${to.name}`
+
+    if (to.name === 'login'){
+        if (store.getters['auth/hasToken']) {
+            next({ name: 'profile' })
+        } else {
+            try{
+                await store.dispatch('auth/ActionCheckToken')
+                next({ name: 'profile' })
+            } catch(error) {
+                if (error != 'Error: Request failed with status code 500') {
+                    next()
+                }
+            }
+        }
+    } else if (to.name !== 'login') {
+        if (store.getters['auth/hasToken']) {
+            next()
+        } else {
+            try {
+                await store.dispatch('auth/ActionCheckToken')
+                next()
+            } catch (error) {
+                if (error != 'Error: Request failed with status code 500') {
+                    next({ name: 'login' })
+                }
+            }
+        }
+    }
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import routes from './routes'
+import beforeEach from './beforeEach'
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+router.beforeEach(beforeEach)
 
 export default router
